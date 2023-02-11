b24e7976116eb4db03e75b95e6a6f3dbde6e6c61
mubarak23 <mubarakaminu340@gmail.com>
2023-02-11 21:11:54 +0100

    change cors config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { initializeTransactionalContext } from 'typeorm-transactional';
 
 async function bootstrap() {
-  initializeTransactionalContext();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('v1/api');
-  app.enableCors();
   await app.listen(3002);
 }
 bootstrap();
