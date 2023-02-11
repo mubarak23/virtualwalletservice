d1c825632a55bd57a7e2214cd716f1fa0a23075b
mubarak23 <mubarakaminu340@gmail.com>
2023-02-11 21:23:07 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    allowedHeaders: '*',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   });
