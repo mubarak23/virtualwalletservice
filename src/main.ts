942a6a1563ea34b1f4becde2629f0a6b5ab74bf0
mubarak23 <mubarakaminu340@gmail.com>
2023-02-11 21:18:59 +0100

    update cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   app.setGlobalPrefix('v1/api');
   await app.listen(3002);
 }
