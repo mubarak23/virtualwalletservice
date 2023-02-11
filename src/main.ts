44b2abac8deba34da363d895cdbaf02812edb213
mubarak23 <mubarakaminu340@gmail.com>
2023-02-11 21:45:16 +0100

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   allowedHeaders: '*',
-  //   origin: '*',
-  //   methods: ['GET', 'POST'],
-  //   credentials: true,
-  // });
-
+  app.setGlobalPrefix('v1/api');
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
-
-  app.setGlobalPrefix('v1/api');
   await app.listen(3000);
 }
 bootstrap();
