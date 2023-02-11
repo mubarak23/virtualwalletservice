5a458b3d315a63e9d7654f9720ea7e79aafb2e9a
mubarak23 <mubarakaminu340@gmail.com>
2023-02-11 22:08:36 +0100

    cors in object

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('v1/api');
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
+  // app.setGlobalPrefix('v1/api');
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   allowedHeaders: 'Content-Type, Accept',
+  // });
   await app.listen(3000);
 }
 bootstrap();
