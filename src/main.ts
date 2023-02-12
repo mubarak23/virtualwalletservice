f764f7aa9c2734a22e7e7a1907e1c12d46acdc83
mubarak23 <mubarakaminu340@gmail.com>
2023-02-12 12:33:47 +0100

    add Authorization in cors header

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
   await app.listen(3002);
 }
